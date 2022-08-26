963abc7770f1171209ed2f5bf780e1cb73fefdb0
Harry-KNIGHT <eliott.knight@icloud.com>
2022-08-26 16:15:50 +0200

    Managing state and app cycle done ✅

--- Scrumdinger/View/MeetingView.swift
+++ Scrumdinger/View/MeetingView.swift
@@ -6,47 +6,50 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct MeetingView: View {
+	@Binding var scrum: DailyScrum
+	@StateObject public var scrumTimer = ScrumTimer()
+	private var player: AVPlayer { AVPlayer.sharedDingPlayer }
+
+
     var body: some View {
-		VStack {
-			ProgressView(value: 5, total: 15)
-			HStack {
-				VStack(alignment: .leading) {
-					Text("Seconds elapsed")
-						.font(.caption)
-					Label("600", systemImage: "hourglass.bottomhalf.fill")
-				}
-				Spacer()
-				VStack(alignment: .trailing) {
-					Text("Seconds remaining")
-						.font(.caption)
+		ZStack {
+			RoundedRectangle(cornerRadius: 16.0)
+				.fill(scrum.theme.mainColor)
+			VStack {
+				MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed, secondsRemaining: scrumTimer.secondsRemaining, theme: scrum.theme)
 
-					Label("600", systemImage: "hourglass.tophalf.fill")
-				}
+				Circle()
+					.strokeBorder(lineWidth: 20)
+
+				MeetingFooterView(speaker: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
+
 			}
-			.accessibilityElement(children: .ignore)
-			.accessibilityLabel("Time remaining")
-			.accessibilityValue("10 minutes")
-			Circle()
-				.strokeBorder(lineWidth: 20)
-
-			HStack {
-				Text("Speaker 1 of 3")
-				Spacer()
-				Button(action: {}, label: {
-					Image(systemName: "forward.fill")
-				})
-				.accessibilityLabel("Next speaker")
-			}
-
 		}
 		.padding()
+		.foregroundColor(scrum.theme.accentColor)
+		.onAppear {
+			scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendees: scrum.attendees)
+			scrumTimer.startScrum()
+
+			scrumTimer.speakerChangedAction = {
+				player.seek(to: .zero)
+				player.play()
+			}
+		}
+		.onDisappear {
+			scrumTimer.stopScrum()
+		}
+		.navigationBarTitleDisplayMode(.inline)
+
     }
 }
 
 struct MeetingView_Previews: PreviewProvider {
+
     static var previews: some View {
-        MeetingView()
+		MeetingView(scrum: .constant(DailyScrum.sampleData[0]))
     }
 }
